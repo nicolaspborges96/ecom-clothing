5d929a153bea11ad17f7488c509466d316295c3f
nicolaspborges96 <nicolaspborges96@gmail.com>
2022-11-02 16:02:24 -0300

    implementado contexto

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,7 @@ import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
+
 import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
@@ -18,8 +19,6 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
-    console.log(formFields)
-
     const resetFormFields = () => {
         setFormFields(defaultFormFields)
     }
@@ -33,7 +32,7 @@ const SignUpForm = () => {
         }
         
         try {
-            const {user} = await createAuthUserWithEmailAndPassword(email, password);
+            const { user } = await createAuthUserWithEmailAndPassword(email, password);
             
             await createUserDocumentFromAuth(user, {displayName});
             resetFormFields();
